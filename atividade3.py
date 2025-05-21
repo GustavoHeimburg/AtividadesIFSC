1d4ce38947039c77f1ef41e14a894e3edb4e6799
IsaFranceski <isadora.df06@aluno.ifsc.edu.br>
2025-05-21 20:08:24 -0300

    Commit

---
+++ atividade3.py
@@ -0,0 +1,6 @@
+altura = float(input("Digite sua altura: "))
+
+if (altura >= 1.50):
+    print("Voce pode andar de montanha russa! sua altura é:",altura)
+else:
+    print("A altura minima é de 1.50 para andar!")
